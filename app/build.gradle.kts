bbe60e5cc60586e1e709a50d7a92b361a642f023
Clement Roncolato <clement.roncolato@epfedu.fr>
2026-04-27 15:20:38 +0200

    Bump Android version code to 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.clementroncolatoCV.cvclementroncolato"
         minSdk = 23
         targetSdk = 35
-        versionCode = 6
+        versionCode = 7
         versionName = "6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
