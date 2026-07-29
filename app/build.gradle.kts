b78a9aa29360f4314f7a1ab9013296cb6e94b2a1
Clement Roncolato <clement.roncolato@epfedu.fr>
2026-07-29 18:17:14 +0200

    Target Android 16 API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "com.clementroncolatoCV.cvclementroncolato"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.clementroncolatoCV.cvclementroncolato"
         minSdk = 23
-        targetSdk = 35
-        versionCode = 7
-        versionName = "7.0"
+        targetSdk = 36
+        versionCode = 8
+        versionName = "8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
